27bdc471c4a8d616117ea2beb547a5e352fd3bb0
dijojgeorge <140865690+dijojgeorge@users.noreply.github.com>
2023-08-06 22:44:46 +0530

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -xe
 export PATH=https://drive.google.com/open?id=10LrCSxYhRjpM9Ur-oFjJjZ142g6-VyRA&usp=drive_fs
 #$PATH:/usr/local/bin
-if [ $( docker ps -a -q --filter ancestor=telecom --format="{{.ID}}" | wc -l ) -gt 0 ]; then
-	docker rm $(docker stop $(docker ps -a -q --filter ancestor=telecom --format="{{.ID}}"))
+if [ $( docker ps -a -q --filter ancestor=77121821061_telecom_churn --format="{{.ID}}" | wc -l ) -gt 0 ]; then
+	docker rm $(docker stop $(docker ps -a -q --filter ancestor=77121821061_telecom_churn --format="{{.ID}}"))
 fi
-docker build -t telecom .
-docker run --name telecom -p 8000:8000 telecom &
+docker build -t 77121821061_telecom_churn .
+docker run --name telecom -p 8000:8000 77121821061_telecom_churn &
